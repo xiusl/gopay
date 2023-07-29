5a8c6626201538d932602edf32c1b066ca5baa38
xiusl <xiushilin@hotmail.com>
2023-07-29 12:44:57 +0800

    feat: client add sdkExecute func

--- alipay/client.go
+++ alipay/client.go
@@ -359,6 +359,33 @@ func (a *Client) PageExecute(ctx context.Context, bm gopay.BodyMap, method strin
 	return baseUrl + "?" + param, nil
 }
 
+func (a *Client) SdkExecute(ctx context.Context, bizContent gopay.BodyMap, method string, authToken ...string) (param string, err error) {
+	pubBody := make(gopay.BodyMap)
+	pubBody.Set("app_id", a.AppId).
+		Set("method", method).
+		Set("format", "JSON").
+		Set("charset", a.Charset).
+		Set("sign_type", a.SignType).
+		Set("version", "1.0").
+		Set("timestamp", time.Now().Format(util.TimeLayout))
+
+	pubBody.Set("app_cert_sn", a.AppCertSN)
+	pubBody.Set("alipay_root_cert_sn", a.AliPayRootCertSN)
+
+	pubBody.Set("biz_content", bizContent)
+
+	sign, err := a.getRsaSign(pubBody, pubBody.GetString("sign_type"), a.privateKey)
+	if err != nil {
+		return "", fmt.Errorf("GetRsaSign Error: %w", err)
+	}
+	pubBody.Set("sign", sign)
+	if a.DebugSwitch == gopay.DebugOn {
+		xlog.Debugf("Alipay_Request: %s", pubBody.JsonBody())
+	}
+	param = pubBody.EncodeURLParams()
+	return
+}
+
 // 公共参数处理
 func (a *Client) pubParamsHandle(bm gopay.BodyMap, method, bizContent string, authToken ...string) (param string, err error) {
 	pubBody := make(gopay.BodyMap)
@@ -519,6 +546,85 @@ func (a *Client) FileRequest(ctx context.Context, bm gopay.BodyMap, file *util.F
 	url := baseUrlUtf8 + "&" + param
 	bm.Reset()
 	bm.SetFormFile("file_content", file)
+	httpClient := xhttp.NewClient()
+	res, bs, err := httpClient.Type(xhttp.TypeMultipartFormData).Post(url).
+		SendMultipartBodyMap(bm).EndBytes(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if a.DebugSwitch == gopay.DebugOn {
+		xlog.Debugf("Alipay_Response: %s%d %s%s", xlog.Red, res.StatusCode, xlog.Reset, string(bs))
+	}
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP Request Error, StatusCode = %d", res.StatusCode)
+	}
+	return bs, nil
+}
+
+// 图片上传
+func (a *Client) UploadImage(ctx context.Context, bm gopay.BodyMap, file *util.File, method string) (bs []byte, err error) {
+	var (
+		bodyStr string
+		bodyBs  []byte
+		aat     string
+	)
+	if bm != nil {
+		aat = bm.GetString("app_auth_token")
+		bm.Remove("app_auth_token")
+		if bodyBs, err = json.Marshal(bm); err != nil {
+			return nil, fmt.Errorf("json.Marshal：%w", err)
+		}
+		bodyStr = string(bodyBs)
+	}
+	pubBody := make(gopay.BodyMap)
+	pubBody.Set("app_id", a.AppId).
+		Set("method", method).
+		Set("format", "JSON").
+		Set("charset", a.Charset).
+		Set("sign_type", a.SignType).
+		Set("version", "1.0").
+		Set("scene", "SYNC_ORDER").
+		Set("timestamp", time.Now().Format(util.TimeLayout))
+
+	if a.AppCertSN != util.NULL {
+		pubBody.Set("app_cert_sn", a.AppCertSN)
+	}
+	if a.AliPayRootCertSN != util.NULL {
+		pubBody.Set("alipay_root_cert_sn", a.AliPayRootCertSN)
+	}
+	if a.ReturnUrl != util.NULL {
+		pubBody.Set("return_url", a.ReturnUrl)
+	}
+	if a.location != nil {
+		pubBody.Set("timestamp", time.Now().In(a.location).Format(util.TimeLayout))
+	}
+	if a.NotifyUrl != util.NULL { //如果返回url为空，传过来的返回url不为空
+		//fmt.Println("url不为空？", a.NotifyUrl)
+		pubBody.Set("notify_url", a.NotifyUrl)
+	}
+	//fmt.Println("notify,", pubBody.JsonBody())
+	if a.AppAuthToken != util.NULL {
+		pubBody.Set("app_auth_token", a.AppAuthToken)
+	}
+	if aat != util.NULL {
+		pubBody.Set("app_auth_token", aat)
+	}
+	if bodyStr != util.NULL {
+		pubBody.Set("biz_content", bodyStr)
+	}
+	sign, err := a.getRsaSign(pubBody, pubBody.GetString("sign_type"), a.privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("GetRsaSign Error: %w", err)
+	}
+	//pubBody.Set("file_content", file.Content)
+	pubBody.Set("sign", sign)
+	if a.DebugSwitch == gopay.DebugOn {
+		xlog.Debugf("Alipay_Request: %s", pubBody.JsonBody())
+	}
+	param := pubBody.EncodeURLParams()
+	url := baseUrlUtf8 + "&" + param
+	bm.Reset()
+	bm.SetFormFile("image_content", file)
 	httpClient := xhttp.NewClient()
 	res, bs, err := httpClient.Type(xhttp.TypeMultipartFormData).Post(url).
 		SendMultipartBodyMap(bm).EndBytes(ctx)
